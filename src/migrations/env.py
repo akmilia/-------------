2ff3f89ddb00a8396f7c237c44da5991be285bf7
akmilia <akmilia@yandex.ru>
2025-03-24 15:59:27 +0300

    now problems with env.py

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -1,39 +1,19 @@
 from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
-
 import sys
-from alembic import context
 
 sys.path.append('../')
-
-# Import your models using the direct package path
 from apps.users.models import Base
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
-
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
 
 target_metadata = Base.metadata 
 
-
-config = context.config
-
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
-
 def run_migrations_offline():
-    # Код для оффлайн-миграций
     pass
 
 def run_migrations_online():
